c21dc7172582dbbc4353addfe716576835dab837
OgawaTakeshi <q2bh3jv@gmail.com>
2025-12-27 21:22:15 +0900

    session4 compete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <ArticleList />
       </section>
 
-      <aside className="w-full md:w-2/3 flex flex-col items-center px-3">
+      <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
         <div className="bg-white shadow-md rounded p-4 mb-6 mt-4">
           <h3 className="font-bold text-gray-900 mb-2">About Me</h3>
           <p className="text-gray-600">
