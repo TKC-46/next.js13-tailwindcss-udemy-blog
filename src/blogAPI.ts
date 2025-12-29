b423c11f6a4ad7539a73655f7de3247d48d6ce76
TKC46 <q2bh3jv@gmail.com>
2025-12-29 11:31:02 +0900

    個別ブログの表示

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,9 +1,36 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
+// ブログ全取得
 export const getAllArticles = async (): Promise<Article[]> => {// force-cache:SSG(ビルド時にサーバーサイドで一度だけレンダリング)
-                                                            // next: {revalidate: 10} ISR(指定した時間ごとにキャッシュを更新する)
-    const res = await fetch(`http://localhost:3001/posts`, {cache: "no-store"});// SSR(リクエスト時にサーバーサイドでレンダリングして返す
+    const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" });// SSR(リクエスト時にサーバーサイドでレンダリングして返す
+
+    if (!res.ok) {
+        throw new Error("エラーが発生しました。");
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 500)); // 擬似的に遅延を発生させる(ロードページ確認のため)
 
     const articles = await res.json();// データをシリアライズ(文字列化)
     return articles;
+}
+
+// ID指定ブログ取得
+export const getDetailArticle = async (id: string): Promise<Article> => {
+    const res = await fetch(`http://localhost:3001/posts/${id}`, {
+        next: { revalidate: 60 }
+    });// ISR(指定した時間ごとにキャッシュを更新する)
+
+    if (res.status === 404) {
+        notFound();
+    }
+
+    if (!res.ok) {
+        throw new Error("エラーが発生しました。");
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    const article = await res.json();
+    return article;
 }
